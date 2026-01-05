9429b870c9970c25b603cf2117f8fda8db017076
lzech <lasse.zech@gmail.com>
2026-01-05 13:18:50 +0100

    added logger.py and drawing of game window

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids with pygame version:", pygame.__version__)
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
